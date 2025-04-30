da71631f27931f657dbc5b5b25c28e627ea8f5ed
iskan <iskan@DESKTOP-34S5QP1>
2025-04-30 22:57:03 +0200

    Add project files.

---
+++ MonotonicStack/MonotonicStack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonotonicStack
+{
+    class MonotonicStack<T> : IEnumerable<T> where T : IComparable<T> 
+    {
+        private Stack<T> _stack;
+        private bool greater = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="greater">If the monotonic stack must be greater or not. By default flase.</param>
+        public MonotonicStack([Optional]bool greater)
+        {
+            _stack = new();
+            this.greater = greater;
+        }
+        public MonotonicStack(IEnumerable<T> enumerator, [Optional]bool greater) : this(greater)
+        {
+            foreach(var item in enumerator)
+            {
+                Push(item);
+            }
+        }
+
+        public int Count => _stack.Count;
+
+        public void Push(T data)
+        {
+            if (Empty())
+            {
+                _stack.Push(data);
+                return;
+            }
+            if (!Compare(data))
+            {
+                _stack.Push(data);
+                return;
+            }
+            while (Compare(data))
+            {
+                Pop();
+            }
+            Push(data);
+        }
+        public T Peek()
+        {
+            return _stack.Peek();
+        }
+        public T Pop()
+        {
+            return _stack.Pop();
+        }
+
+        public bool Empty() => (Count == 0);
+        private bool Compare(T data)
+        {
+            if (Empty()) return false;
+            if (greater) return _stack.Peek().CompareTo(data) > 0;
+            return data.CompareTo(_stack.Peek())>0;
+        }
+
+        public IEnumerator<T> GetEnumerator() => _stack.GetEnumerator();
+        
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+    }
+}
---
+++ MonotonicStack/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MonotonicStack;
+int[] v = { 1, 2, 3, 8, 5, 6, 7, 8, 1 };
+var monotonicStack = new MonotonicStack<int>(v, false);
+
+while (!monotonicStack.Empty())
+    Console.WriteLine(monotonicStack.Pop());
+
+
+
